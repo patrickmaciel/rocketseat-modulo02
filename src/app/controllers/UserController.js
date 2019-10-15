1b152256128efb689ddc7d8d6ec735148bad0bc6
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-15 00:13:34 -0300

    Gerando hash da senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,13 +2,6 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    // Não preciso fazer isso pois o Schema já trata isso
-    // const { name, email, password_hash } = req.body;
-    // const user = await User.create({
-    //   name,
-    //   email,
-    //   password_hash
-    // })
     const userExists = await User.findOne({ where: { email: req.body.email }});
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
