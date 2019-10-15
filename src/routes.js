5d0d09d12418c82c57dbbcf6da08aa7288154707
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-15 00:02:37 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Jesus é Santo' })
-})
+routes.get('/', async(req, res) => {
+  const user = await User.create({
+    name: 'Patrick Maciel',
+    email: 'patrickmaciel.info@gmail.com',
+    password_hash: '123412341234',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
