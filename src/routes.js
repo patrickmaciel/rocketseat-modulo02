2bde5a330c614ac907f02a437f0d260b480654bc
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-15 22:41:20 -0300

    Fix - eslint

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
-import authMiddleware from './app/middlewares/auth'
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
