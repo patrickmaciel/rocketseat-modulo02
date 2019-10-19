c632ee6eb3fa0a682cf63562dd5b113bb6a6af2b
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-19 17:02:09 -0300

    Avatar do usuário

---
+++ src/database/migrations/20191019195135-add-avatar-field-to-users.js
@@ -0,0 +1,18 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'files',
+        key: 'id',
+        onUpdate: 'SET NULL',
+        onDelete: 'CASCADE',
+        allowNull: true,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn('users', 'avatar');
+  },
+};
