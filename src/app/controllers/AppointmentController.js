cd749c80587ce26474a7968b24a2027ce3395f47
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-20 14:47:18 -0300

    Configurando templates de email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -120,6 +120,7 @@ class AppointmentController {
     const appointment = await Appointment.findByPk(req.params.id, {
       include: [
         { model: User, as: 'provider', attributes: ['name', 'email', 'id'] },
+        { model: User, as: 'user', attributes: ['name', 'email', 'id'] },
       ],
     });
     if (!appointment) {
@@ -145,7 +146,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' d 'de' MMMM', às 'H:mm'h", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
